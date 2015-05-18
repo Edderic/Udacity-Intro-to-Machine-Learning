08af243349aa6ca5e65914bf745cd678f021996d
Edderic Ugaddan <edderic@gmail.com>
2015-05-17 21:55:44 -0400

    [#94753446] use long_term_incentive as a feature

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,10 +10,11 @@ from tester import test_classifier, dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'total_payments', 'exercised_stock_options', 'bonus'] # You will need to use more features
+features_list = ['poi','salary', 'long_term_incentive', 'total_payments', 'exercised_stock_options', 'bonus'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
+# print data_dict
 
 ### Task 2: Remove outliers
 data_dict.pop('TOTAL')
